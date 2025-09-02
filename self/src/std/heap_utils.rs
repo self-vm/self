6fbd892446d99aa7211d2ad596cbc2981765609f
noreplydev <contact.cristiansanchez@gmail.com>
2025-09-02 22:49:44 +0200

    add put_vector to heap_utils methods

--- self/src/std/heap_utils.rs
+++ self/src/std/heap_utils.rs
@@ -1,8 +1,13 @@
 use crate::{
     memory::{Handle, MemObject},
+    types::{object::vector::Vector, Value},
     vm::Vm,
 };
 
 pub fn put_string(vm: &mut Vm, string: String) -> Handle {
     vm.memory.alloc(MemObject::String(string))
+}
+
+pub fn put_vector(vm: &mut Vm, vector: Vec<Value>) -> Handle {
+    vm.memory.alloc(MemObject::Vector(Vector::new(vector)))
 }
