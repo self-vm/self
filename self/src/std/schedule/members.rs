fa0a7e5fbaaefac1163f8e58d53fb3684feeb157
noreplydev <contact.cristiansanchez@gmail.com>
2025-12-22 19:28:26 +0100

    add timeout method to the schedule module

--- self/src/std/schedule/members.rs
+++ self/src/std/schedule/members.rs
@@ -1,7 +1,7 @@
 use std::time::Duration;
 
 use futures::future::BoxFuture;
-use tokio::time::interval as tokio_interval;
+use tokio::time::{interval as tokio_interval, sleep};
 
 use crate::{
     core::error::{self, type_errors::TypeError, VMError, VMErrorType},
@@ -53,7 +53,7 @@ pub fn interval(
             println!("INTERVAL -> {}", milliseconds)
         }
 
-        let mut tick = tokio_interval(Duration::from_secs((milliseconds / 1000) as u64));
+        let mut tick = tokio_interval(Duration::from_millis(*milliseconds as u64));
         let interval_struct = Interval::new_initialized(vm);
         let interval_obj_handle = vm
             .memory
@@ -64,11 +64,55 @@ pub fn interval(
         tokio::spawn(async move {
             loop {
                 tick.tick().await;
-                vm_notifier.send(Event::Call(callback.clone()));
+                let _ = vm_notifier.send(Event::Call(callback.clone()));
             }
         });
 
         Ok(Value::Handle(interval_obj_handle))
+    })
+}
+
+pub fn timeout_obj() -> MemObject {
+    MemObject::Function(Function::new(
+        "timeout".to_string(),
+        vec!["callback".to_string(), "milliseconds".to_string()],
+        Engine::NativeAsync(timeout),
+    ))
+}
+
+pub fn timeout(
+    vm: &mut Vm,
+    _self: Option<Handle>,
+    params: Vec<Value>,
+    debug: bool,
+) -> BoxFuture<'_, Result<Value, VMError>> {
+    Box::pin(async move {
+        if params.len() < 2 {
+            return Err(error::throw(
+                VMErrorType::TypeError(TypeError::InvalidArgsCount {
+                    expected: 2,
+                    received: params.len() as u32,
+                }),
+                vm,
+            ));
+        }
+
+        let callback = &params[0].as_function_obj(vm)?;
+        let callback = callback.clone();
+        let milliseconds = &params[1].as_usize(vm)?;
+        let vm_notifier = vm.get_vm_notifier();
+
+        if debug {
+            println!("TIMEOUT -> {}", milliseconds)
+        }
+
+        let timeout_millis = Duration::from_millis(*milliseconds as u64);
+        tokio::spawn(async move {
+            sleep(timeout_millis).await;
+            let _ = vm_notifier.send(Event::Call(callback.clone()));
+        });
+
+        Ok(Value::RawValue(RawValue::Nothing))
     })
 }
 
--- self/src/std/schedule/mod.rs
+++ self/src/std/schedule/mod.rs
@@ -1,11 +1,15 @@
 mod members;
 pub mod types;
-use crate::{memory::MemObject, std::schedule::members::interval_obj};
+use crate::{
+    memory::MemObject,
+    std::schedule::members::{interval_obj, timeout_obj},
+};
 
 pub fn generate_struct() -> (String, Vec<(String, MemObject)>) {
     let mut fields = vec![];
 
     fields.push(("interval".to_string(), interval_obj()));
+    fields.push(("timeout".to_string(), timeout_obj()));
 
     ("schedule".to_string(), fields)
 }
