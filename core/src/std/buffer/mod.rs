57ebbf0f3b4bd76df4266e26b7ddfe18e4431ec9
noreplydev <contact.cristiansanchez@gmail.com>
2026-01-09 20:52:11 +0100

    add from_bytes method to buffer

--- core/src/std/buffer/mod.rs
+++ core/src/std/buffer/mod.rs
@@ -22,11 +22,14 @@ pub mod types;
 pub fn init_constructor(vm: &mut Vm) -> MemObject {
     let mut fields = HashMap::new();
     let from_byte_handle = vm.memory.alloc(from_byte_obj());
+    let from_bytes_handle = vm.memory.alloc(from_bytes_obj());
 
     fields.insert("from_byte".to_string(), Value::Handle(from_byte_handle));
+    fields.insert("from_bytes".to_string(), Value::Handle(from_bytes_handle));
     MemObject::StructLiteral(StructLiteral::new("Buffer".to_string(), fields))
 }
 
+// from_byte
 fn from_byte_obj() -> MemObject {
     MemObject::Function(Function::new(
         "from_byte".to_string(),
@@ -43,6 +46,37 @@ pub fn from_byte(
 ) -> Result<Value, VMError> {
     let byte = params[0].as_primitive(vm, DataType::Byte)?.as_byte(vm)?;
     let buf = Buffer::new_initialized(vec![byte.value], vm);
+    let buf_handle = vm
+        .memory
+        .alloc(MemObject::NativeStruct(NativeStruct::Buffer(buf)));
+
+    Ok(Value::Handle(buf_handle))
+}
+
+// from_bytes
+fn from_bytes_obj() -> MemObject {
+    MemObject::Function(Function::new(
+        "from_bytes".to_string(),
+        vec!["bytes vector".to_string()],
+        Engine::Native(from_bytes),
+    ))
+}
+
+pub fn from_bytes(
+    vm: &mut Vm,
+    _self: Option<Handle>,
+    params: Vec<Value>,
+    debug: bool,
+) -> Result<Value, VMError> {
+    let bytes = params[0].as_vector_obj(vm)?;
+    let bytes = bytes
+        .elements
+        .iter()
+        .map(|v| -> Result<u8, VMError> {
+            Ok(v.as_primitive(vm, DataType::Byte)?.as_byte(vm)?.value)
+        })
+        .collect::<Result<Vec<u8>, VMError>>()?;
+    let buf = Buffer::new_initialized(bytes, vm);
     let buf_handle = vm
         .memory
         .alloc(MemObject::NativeStruct(NativeStruct::Buffer(buf)));
